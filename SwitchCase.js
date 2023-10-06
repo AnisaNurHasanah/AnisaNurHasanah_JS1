d7ca16047be181c8e396aab9f1e751d20baee5a8
anisanurhasanah <2010631170052@student.unsika.ac.id>
2023-10-06 14:41:13 +0700

    first commit

---
+++ SwitchCase.js
@@ -0,0 +1,22 @@
+let warna = prompt ('masukan pilihan warnamu: ');
+
+switch (warna) {
+case 'merah' :
+    alert('Wah pilihan warnamu sangat berani');
+    break;
+case 'kuning' :
+    alert('Menarik! kuning melambangkan keceriaan');
+    break;
+case 'hijau' :
+    alert('Keren! hijau merupakan warna yang sangat indah');
+    break;
+case 'hitam' :
+    alert('Menakjubkan! tidak selamanya hitam melambangkan duka, karena bintang yang indah pun bersinar ditengah kegelapan');
+    break;
+case 'biru' :
+    alert('Aku juga sangat warna biru karena mengingatkanku dengan laut');
+    break;
+default :
+    alert('Maaf, sayang sekali warna yang kamu pilih tidak tersedia');
+    break;
+}
